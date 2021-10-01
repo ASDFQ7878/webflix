21e90be1f9d4ead68c3a2a3629d94eab2eb31434
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 20:33:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php  
+
+include("header.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEBFLIX</title>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/51dcd449b3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  
+
+
+
+</head>
+<body>
+
+<section class="header-section">
+  <div class="center-div">
+       <h1 class="font-weight-bold">WELCOME TO <span class="colour">FLUSS AQUA</span></h1>
+       <p> We Provides The Worlds Best Purified Water. </p>
+      <div class="header-buttons">
+        <a href="#servicediv">Play</a>
+        <a href="#contactid">mute</a>
+      </div>
+  </div>
+ </section>
+</div>
+
+
+<div class="col-lg-12"><h3>Action</h3></div>	
+<div class="categories col-lg-12 row " >
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+</div>
+
+
+<div class="col-lg-12"><h3>Action</h3></div>	
+<div class="categories col-lg-12 row " >
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+<div class="col-md-3" >
+	<img src="images/KGF.jfif" style="width: 200px; height: 150px;">
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEBFLIX</title>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/51dcd449b3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+ <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item active">
+        <a class="nav-link"  href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#servicediv">TV Shows</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#pricingdiv">Movies</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#project-work"><i class="fas fa-search"></i></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="profile.php"><i class="fas fa-user"></i></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#contactid"><i class="fas fa-sign-out-alt"></i></a>
+      </li>
+    </ul>
+  </div>
+ </div>
+ </nav>
+
+    
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
